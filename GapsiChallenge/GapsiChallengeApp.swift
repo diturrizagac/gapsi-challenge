21254d0abb5cc2678a98257b9d5003875f6800fc
Diego Iturrizaga <diturrizagac@gmail.com>
2025-06-16 11:32:46 -0500

    Initial Commit

---
+++ GapsiChallenge/GapsiChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GapsiChallengeApp.swift
+//  GapsiChallenge
+//
+//  Created by Diego Iturrizaga on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GapsiChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GapsiChallengeTests/GapsiChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GapsiChallengeTests.swift
+//  GapsiChallengeTests
+//
+//  Created by Diego Iturrizaga on 16/06/25.
+//
+
+import Testing
+@testable import GapsiChallenge
+
+struct GapsiChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
